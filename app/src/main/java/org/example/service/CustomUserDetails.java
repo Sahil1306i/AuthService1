fc9145a67e819eb30bf0c59974eb83c9dff0a20c
sahil <www.sgt1625@gmail.com>
2025-08-15 18:22:29 +0530

    Initial commit

---
+++ app/src/main/java/org/example/service/CustomUserDetails.java
@@ -0,0 +1,73 @@
+package org.example.service;
+
+import org.example.entities.UserInfo;
+import org.example.entities.UserRole;
+import org.hibernate.sql.ast.tree.expression.Collation;
+import org.modelmapper.Converters;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+public class CustomUserDetails extends UserInfo implements UserDetails {
+    //interfaces are implemented instead of extended
+    //extend means that this class will have all the atributes of the class which is extended to this class
+
+
+    private String username;
+    private String password;
+
+    Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetails(UserInfo byUsername) {
+        this.username = byUsername.getUsername();
+        this.password = byUsername.getPassword();
+        List<GrantedAuthority> auths = new ArrayList<>();
+
+        for(UserRole role : byUsername.getRoles()){
+            auths.add(new SimpleGrantedAuthority(role.getName().toUpperCase()));
+            //roles are added to the auth in a loop
+        }
+        this.authorities = auths;
+    }
+
+    
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
---
+++ app/src/main/java/org/example/service/JwtService.java
@@ -0,0 +1,50 @@
+package org.example.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+
+public class JwtService {
+    public static final String SECRET = "b7xfrYehxb++FrpC+vAdNELwYmqkcsqOcYVD54gPgkVdKElDrOFgYgYq/va141HmWUzkoPKfzMjpvGH+eJ0vwOe/I/JpvLXoFMgqtJ3oyeYAFdTA7J8SH5UvTkLCL6kUOcaXN3fkpMyY8XMevK4a28BnomaryQUkD4IVUNnBQ9+p+yang2AZrLZoBMNAwsw5LMkQK1XoCKZJevoFCjyWlS3CZXIHsv/Gm23Z03ape4dM8OCNjDkmY4c+fYVxGq10VDU0pYcsi8VRehexe34s1FAg7gfNLAO03c3Yxl3kaZmw7KLvbqojYcKZ8yNgSdFc72ZF0gHeFkSxay2v0RpvrnKSZ+5ox8l0/Y6a7oLOL4E=\n"
+
+    public String extractUsername(String token){
+        // here we got a token then we made a claim for the token
+        return extractClaim(token, Claims::getSubject);
+        // returns the JWT subject value or null  if not present
+         }
+         //T here means that we can pass any data type here
+
+         public <T> T extractClaim(String token, Function<Claims , T> claimResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+         }
+         
+         public Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+         }
+
+         private Claims extractAllClaims(String token){
+        return Jwts
+                .parser()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJwt(token)//gives the token to the parser and then makes the jwt for the parser
+                .getBody();
+                //chaining something i guess
+         }
+
+         private Key getSignKey(){
+        byte[] keyBytes = Base64.Decoder.BASE64.decode(SECRET);
+        //decoding the secret key then making a new key instance
+        return Keys.hmacShaKeyFor(keyBytes);
+         }
+
+}
